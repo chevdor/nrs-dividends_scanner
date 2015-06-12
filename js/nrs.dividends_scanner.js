27b82eaa124f3b8bc5505808aad4f4b8d8f35f70
Wilfried Kopp <chevdor@gmail.com>
2015-06-13 00:02:11 +0200

    Initial commit. It works!

---
+++ js/nrs.dividends_scanner.js
@@ -0,0 +1,237 @@
+/**
+ * @depends {nrs.js}
+ */
+var NRS = (function(NRS, $, undefined) {
+    var sortByAggregatedIncomeAmount = function(a, b) {
+        var x = a.aggregated.transactedAmountNQT;
+        var y = b.aggregated.transactedAmountNQT;
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    };
+
+    var findIndexOfSender = function(array, senderRS) {
+        for (i = 0; i < array.length; i++) {
+            if (array[i].senderRS === senderRS)
+                return i;
+        }
+        return null;
+    };
+
+    var getAsset = function(id, cb) {
+        NRS.sendRequest("getAsset+", {
+            "asset": id.toString()
+        }, function(asset, input) {
+            cb(asset);
+        });
+    };
+
+    var getAssetBidPrice = function(id, cb) {
+        NRS.sendRequest("getBidOrders+", {
+            "asset": id.toString(),
+            "firstIndex": 0,
+            "lastIndex": 0
+        }, function(response, input) {
+            if (response.bidOrders.length > 0)
+                cb(response.bidOrders[0]);
+            else
+                cb(null);
+        });
+    };
+
+    var drawChart = function(name, contentData, size) {
+        //console.log($('#pieChart')); 
+        // first we empty any remaining pie
+        $('#' + name).empty();
+
+        var pie = new d3pie(name, {
+            "header": {
+                "title": {
+                    "text": "Asset Distribution Chart",
+                    "fontSize": 22,
+                    "font": "verdana"
+                },
+                "subtitle": {
+                    "text": "",
+                    "color": "#999999",
+                    "fontSize": 10,
+                    "font": "verdana"
+                },
+                "titleSubtitlePadding": 12
+            },
+            // "footer": {
+            //     "text": "",
+            //     "color": "#999999",
+            //     "fontSize": 11,
+            //     "font": "open sans",
+            //     "location": "bottom-center"
+            // },
+            "size": size || {
+                "canvasHeight": 400,
+                "canvasWidth": 600,
+                "pieOuterRadius": "80%"
+            },
+            data: {
+                "sortOrder": "value-desc",
+                "smallSegmentGrouping": {
+                    "enabled": true,
+                    "value": 1
+                },
+                content: contentData
+            },
+            "labels": {
+                "outer": {
+                    "pieDistance": 32
+                },
+                "inner": {
+                    "format": "value"
+                },
+                "mainLabel": {
+                    "font": "verdana"
+                },
+                "percentage": {
+                    "color": "#e1e1e1",
+                    "font": "verdana",
+                    "decimalPlaces": 0
+                },
+                "value": {
+                    "color": "#e1e1e1",
+                    "font": "verdana"
+                },
+                "lines": {
+                    "enabled": true,
+                    "color": "#cccccc"
+                },
+                "truncation": {
+                    "enabled": true
+                }
+            },
+            "tooltips": {
+                "enabled": true,
+                "type": "placeholder",
+                "string": "{label}: {percentage}%"
+            } //,
+            // "effects": {
+            //     "pullOutSegmentOnClick": {
+            //         "effect": "linear",
+            //         "speed": 400,
+            //         "size": 8
+            //     }
+            // }
+        });
+    };
+
+    var draw = function(dataContent) {
+        if (dataContent.length === NRS.accountInfo.assetBalances.length) {
+            //console.log('Drawing...');
+            //
+            $('#my_assets_page>.content').prepend('<div id="insertedAssetDistributionChart"></div>');
+            drawChart('assetDistributionChart', dataContent);
+            // drawChart('insertedAssetDistributionChart', dataContent, {
+            //     "canvasHeight": 200,
+            //     "canvasWidth": 300,
+            //     "pieOuterRadius": "100%"
+            // }); // we had the chart in the highjacked area ;)
+            NRS.dataLoaded();
+        } else {
+            //console.log('Need more data to draw', dataContent.length, NRS.accountInfo.assetBalances.length);
+        }
+    };
+
+    NRS.setup.p_dividends_scanner = function() {
+        //console.info('NRS.setup.p_dividends_scanner');
+        //Do one-time initialization stuff here
+        $('#p_dividends_scanner_startup_date_time').html(moment().format('LLL'));
+    };
+
+
+    NRS.pages.p_dividends_scanner = function() {
+        var rows = "";
+        var dataContent = [];
+
+        // Asset distribution
+        $.each(NRS.accountInfo.assetBalances, function(field, obj) {
+            //console.log(obj.asset, obj.balanceQNT);
+            getAsset(obj.asset, function(asset) {
+                if (asset) {
+                    console.log('Processing ' + asset.name);
+                    getAssetBidPrice(asset.asset, function(order) {
+                        var price;
+                        if (order)
+                            price = order.priceNQT;
+                        else
+                            price = 1; // cheating a bit for d3pie... 1e-8 is almost 0 anyway...
+
+                        //console.log(asset.name, obj.balanceQNT, price, asset.decimals);
+                        dataContent.push({
+                            label: asset.name,
+                            value: parseInt(obj.balanceQNT) * price / (Math.pow(10, 8))
+                        });
+
+                        draw(dataContent);
+                    });
+                }
+            });
+        });
+
+        // INCOME ANALYSIS
+        NRS.sendRequest("getAccountTransactions+", {
+            "account": NRS.accountInfo.accountRS,
+            "timestamp": 0,
+            "type": 0,
+            "subtype": 0
+        }, function(response) {
+            //console.log(response);
+
+            var transactions = response.transactions;
+            var h = []; // [{sender: XYZ, transactions: [], aggregated: {nbTransactions: xx, transactedAmount: }}]
+
+            // sort transactions per senderRS
+            $.each(transactions, function(i, t) {
+                var ind = findIndexOfSender(h, t.senderRS);
+                var item;
+
+                if (ind === null) {
+                    item = {
+                        senderRS: t.senderRS,
+                        transactions: [],
+                        aggregated: {
+                            nbTransactions: 0,
+                            transactedAmountNQT: 0
+                        }
+                    };
+                    item.transactions.push(t);
+                    item.aggregated.nbTransactions += 1;
+                    item.aggregated.transactedAmountNQT += t.amountNQT * 1e-8;
+
+                    h.push(item);
+                } else {
+                    item = h[ind];
+                    item.transactions.push(t);
+                    item.aggregated.nbTransactions += 1;
+                    item.aggregated.transactedAmountNQT += t.amountNQT * 1e-8;
+                }
+
+                if (i >= transactions.length - 1) {
+                    h.sort(sortByAggregatedIncomeAmount);
+                    h.reverse();
+                    //console.log(h);
+
+                    var chartData = [];
+                    for (var j = 0; j < h.length; j++) {
+                        chartData.push({
+                            "label": h[j].senderRS,
+                            "value": h[j].aggregated.transactedAmountNQT
+                        });
+                    }
+                    drawChart('incomeDistributionChart', chartData);
+                }
+            });
+        });
+
+        $('.data-empty-container').html('');
+    };
+
+    return NRS;
+}(NRS || {}, jQuery));
+
+//File name for debugging (Chrome/Firefox)
+//@ sourceURL=nrs.dividends_scanner.js
